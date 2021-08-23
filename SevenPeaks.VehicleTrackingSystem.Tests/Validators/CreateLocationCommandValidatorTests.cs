1da17be8b2491b8fd2461cb3cd841abb1ae692a2
Nigus <nigussmeani@rideau.com>
2021-08-23 09:28:29 -0500

    Create location validator test is created.

---
+++ SevenPeaks.VehicleTrackingSystem.Tests/Validators/CreateLocationCommandValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts;
+using Application.Contracts.Repositories;
+using Application.Features.Locations.Commands;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenPeaks.VehicleTrackingSystem.Tests.Validators
+{
+    public class CreateLocationCommandValidatorTests
+    {
+        private readonly Mock<IVehicleRepository> _vehicleRepositoryMock;
+        private readonly Mock<ILoggedInUserService> _loggedInUserServiceMock;
+        private readonly CreateLocationCommandValidator _createLocationCommandValidator;
+        public CreateLocationCommandValidatorTests()
+        {
+            _vehicleRepositoryMock = new Mock<IVehicleRepository>();
+            _loggedInUserServiceMock = new Mock<ILoggedInUserService>();
+            _createLocationCommandValidator = new CreateLocationCommandValidator(_vehicleRepositoryMock.Object, _loggedInUserServiceMock.Object);
+        }
+    }
+}
